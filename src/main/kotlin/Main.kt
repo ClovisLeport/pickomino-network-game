a6f2ad5c94c19fdbadb6c97f9dc651b0bce8b2a0
E228541P <clovis.leport@etu.univ-nantes.fr>
2023-06-13 15:01:09 +0200

    package

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import iut.info1.pickomino.Connector
 
+
+
 fun main() {
     println("Hello students !!!")
     val connect = Connector.factory("172.26.82.76", "8080")
@@ -7,6 +9,7 @@ fun main() {
     val identification = connect.newGame(3)
     val id = identification.first
     val key = identification.second
+
     val currentGame = connect.gameState(id, key)
     println("Nouvelle partie = $currentGame")
 }
