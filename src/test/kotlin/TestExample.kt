0c01ebc196b92c73bac5657f294964b1e4ada18b
Clovis LEPORT <E228541P@univ-nantes.fr>
2023-06-13 09:04:24 +0200

    test Dice

--- src/test/kotlin/TestExample.kt
+++ src/test/kotlin/TestExample.kt
@@ -1,10 +1,70 @@
+import iut.info1.pickomino.data.DICE
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class TestExample {
 
+
     @Test
-    fun aTest() {
-        assertEquals(2, 1+1)
+    fun testDice1() {
+        var d1 = DICE.d1
+        assertEquals(0 ,d1.ordinal)
+    }
+    @Test
+    fun testDice1Name() {
+        var d1 = DICE.d1
+        assertEquals("d1" ,d1.name)
+    }
+    @Test
+    fun testDice2() {
+        var d1 = DICE.d2
+        assertEquals(1 ,d1.ordinal)
+    }
+    @Test
+    fun testDice2Name() {
+        var d1 = DICE.d2
+        assertEquals("d2" ,d1.name)
+    }
+    @Test
+    fun testDice3() {
+        var d1 = DICE.d3
+        assertEquals(2,d1.ordinal)
+    }
+    @Test
+    fun testDice3Name() {
+        var d1 = DICE.d3
+        assertEquals("d3" ,d1.name)
+    }
+    @Test
+    fun testDice4() {
+        var d1 = DICE.d4
+        assertEquals(3 ,d1.ordinal)
+    }
+    @Test
+    fun testDice4Name() {
+        var d1 = DICE.d4
+        assertEquals("d4" ,d1.name)
+    }
+
+    @Test
+    fun testDice5() {
+        var d1 = DICE.d5
+        assertEquals(4 ,d1.ordinal)
+    }
+    @Test
+    fun testDice5Name() {
+        var d1 = DICE.d5
+        assertEquals("d5" ,d1.name)
+    }
+
+    @Test
+    fun testDiceWorm() {
+        var d1 = DICE.worm
+        assertEquals(5 ,d1.ordinal)
+    }
+    @Test
+    fun testDiceWormName() {
+        var d1 = DICE.worm
+        assertEquals("worm" ,d1.name)
     }
 }
