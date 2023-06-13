e9add552be2f2fa06ffa1f5d97a56ef31cf596d6
E228541P <clovis.leport@etu.univ-nantes.fr>
2023-06-13 15:27:35 +0200

    player class

--- src/main/kotlin/modele/Game.kt
+++ src/main/kotlin/modele/Game.kt
@@ -1,4 +1,5 @@
 package modele
 
 class Game {
+
 }
---
+++ src/main/kotlin/modele/Player.kt
@@ -0,0 +1,42 @@
+package modele
+
+import iut.info1.pickomino.exceptions.IncorrectKeyException
+
+class Player(id:Int, name:String) {
+    private var id = id
+    private var name = name
+    private var ListPickomino = mutableListOf<Pickomino>()
+    private var diceKept = mutableListOf<Dice>()
+    private var dicePlayed = mutableListOf<Dice>()
+    public fun countWorms():Int{
+        var nb_worms = 0
+        for (i in 0..ListPickomino.size-1) {
+            nb_worms +=ListPickomino[i].nbWorms
+        }
+        return nb_worms
+    }
+
+    public fun firstElement():Pickomino {
+        return ListPickomino[-1]
+    }
+
+    public fun addElement(Picko: Pickomino) {
+        if (ListPickomino.size == 16) {
+            return
+        }else {
+            ListPickomino.add(Picko)
+        }
+    }
+
+    public fun removeElement():Pickomino {
+        var picko = ListPickomino[-1]
+        try {
+            ListPickomino.removeAt(ListPickomino.size-1)
+        }
+        catch (e:IndexOutOfBoundsException) {
+            throw  IncorrectKeyException()
+        }
+        return picko
+    }
+
+}
