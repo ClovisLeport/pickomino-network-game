16f8aca1404f0ec504fa8e776efd75c94e8e215a
lancelot <jouault.lancelot@gmail.com>
2023-06-13 16:40:30 +0200

    add test

---
+++ src/test/kotlin/modele/PickominoTest.kt
@@ -0,0 +1,17 @@
+package modele
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class PickominoTest {
+    <di
+
+    @Test
+    fun getValue() {
+    }
+
+    @Test
+    fun getnbWorm() {
+    }
+}
---
+++ src/test/kotlin/modele/PlayerTest.kt
@@ -0,0 +1,24 @@
+package modele
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class PlayerTest {
+
+    @Test
+    fun countWorms() {
+    }
+
+    @Test
+    fun firstElement() {
+    }
+
+    @Test
+    fun addElement() {
+    }
+
+    @Test
+    fun removeElement() {
+    }
+}
