fcb681007d2e62999e15ac6f3410a44e282c7899
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2023-06-14 12:16:35 +0200

    Button transparant

--- src/main/kotlin/view/game/GameVue.kt
+++ src/main/kotlin/view/game/GameVue.kt
@@ -1,15 +1,12 @@
 package view.game
 
-import javafx.scene.layout.Background
-import javafx.scene.layout.BackgroundFill
-import javafx.scene.layout.GridPane
-import javafx.scene.layout.VBox
+import javafx.geometry.Insets
+import javafx.scene.layout.*
 import javafx.scene.paint.Color
 import view.components.SmallTitle
 import view.components.TransparentButton
-import java.awt.Label
 
-class GameVue : GridPane() {
+class GameVue : BorderPane() {
     // HEADER
     val header : VBox
     val pageTitle : SmallTitle
@@ -20,11 +17,17 @@ class GameVue : GridPane() {
         this.background = Background(BackgroundFill(Color.web("#3A83C2"), null, null))
         // HEADER Init
         this.header = VBox()
+        this.header.style = "-fx-alignment: center;"
+
         this.pageTitle = SmallTitle("Pickomino")
+
         this.menuButton = TransparentButton("Menu")
+        val margin = Insets(8.0, 0.0, 0.0, 0.0)
+        VBox.setMargin(menuButton, margin)
+
         this.header.children.addAll(pageTitle,menuButton)
 
         // Add to page
-        this.children.addAll(header)
+        this.top = header
     }
 }
